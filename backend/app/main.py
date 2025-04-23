09404f319b2767f890822b0468853028fd924389
Rafferty Chen <raffertychen@gmail.com>
2025-04-22 21:31:32 -0700

    Switch to starlette-admin

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,13 +3,13 @@ import sys
 from contextlib import asynccontextmanager
 
 import uvicorn
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-from app.routers.inventory import router as inventory_router
 from app.config import settings
 from app.database import sessionmanager
-
+from app.models import Lenses
+from app.routers.inventory import router as inventory_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette_admin.contrib.sqla import Admin, ModelView
 
 logging.basicConfig(stream=sys.stdout, level=settings.log_level)
 
@@ -29,18 +29,9 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(inventory_router)
 
-origins = [
-    "http://localhost:5173",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["Content-Range"],
-)
+admin = Admin(sessionmanager._engine, title="Test")
+admin.add_view(ModelView(Lenses))
+admin.mount_to(app)
 
 
 @app.get("/")
--- frontend/inventory/src/toolbar.tsx
+++
@@ -1,8 +0,0 @@
-import { DeleteButton, Toolbar, SaveButton } from "react-admin";
-
-export const CustomEditToolbar = () => (
-  <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-    <SaveButton />
-    <DeleteButton mutationMode="pessimistic" />
-  </Toolbar>
-);
