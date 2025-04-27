9e1e0a499d9fcd3376cff41ced745fd24736cb9a
Rafferty Chen <raffertychen@gmail.com>
2025-04-26 20:50:46 -0700

    Update CORS headers

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["Content-Range"],
+    expose_headers=["X-Total-Count"],
 )
 
 
