a55a85aa35c19232eeb0cb936e9a95c0aea4f5e5
Rafferty Chen <raffertychen@gmail.com>
2025-05-20 18:18:06 -0700

    Refactor edit, create, and show views

--- frontend/inventory/src/pages/lenses/create.tsx
+++ frontend/inventory/src/pages/lenses/create.tsx
@@ -1,58 +1,38 @@
-import { useForm } from "@refinedev/core";
+import { useForm, useSelect, Create } from "@refinedev/antd";
+
+import { Form, Input, Select, InputNumber } from "antd";
 
 export const LensCreate = () => {
-  const { onFinish, mutation } = useForm();
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Using FormData to get the form values and convert it to an object.
-    const data = Object.fromEntries(new FormData(event.currentTarget).entries());
-    // Calling onFinish to submit with the data we've collected from the form.
-    onFinish({
-      ...data,
-      sphere: Number(data.sphere),
-      cylinder: Number(data.cylinder),
-      unit_price: Number(data.unit_price).toFixed(2),
-      quantity: data.quantity === "" ? 0 : Number(data.quantity),
-      storage_limit: data.storage_limit === "" ? null : Number(data.storage_limit),
-    });
-  };
+  const { formProps, saveButtonProps } = useForm();
 
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="id">Id</label>
-      <input type="text" id="id" name="id" required />
-
-      <label htmlFor="lens_type">Lens Type</label>
-      <input type="text" id="lens_type" name="lens_type" required />
-
-      <label htmlFor="sphere">Sphere</label>
-      <input type="number" id="sphere" name="sphere" step=".01" required />
-
-      <label htmlFor="cylinder">Cylinder</label>
-      <input type="number" id="cylinder" name="cylinder" step=".01" required />
-
-      <label htmlFor="unit_price">Unit Price</label>
-      <input
-        type="number"
-        id="unit_price"
-        name="unit_price"
-        pattern="\d*\.?\d*"
-        step=".01"
-        required
-      />
-
-      <label htmlFor="quantity">Quantity</label>
-      <input type="number" id="quantity" name="quantity" />
-
-      <label htmlFor="storage_limit">Storage Limit</label>
-      <input type="number" id="storage_limit" name="storage_limit" />
-
-      <label htmlFor="comment">Comment</label>
-      <textarea id="comment" name="comment" />
-
-      {mutation.isSuccess && <span>successfully submitted!</span>}
-      <button type="submit">Submit</button>
-    </form>
+    <Create saveButtonProps={saveButtonProps} title="Create Lens">
+      <Form {...formProps} layout="vertical">
+        <Form.Item label="ID" name="id" rules={[{ required: true }]}>
+          <InputNumber controls={false} style={{ width: "100%" }} />
+        </Form.Item>
+        <Form.Item label="Lens Type" name="lens_type" rules={[{ required: true }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="Sphere" name="sphere" rules={[{ required: true }]}>
+          <InputNumber step="0.01" />
+        </Form.Item>
+        <Form.Item label="Cylinder" name="cylinder" rules={[{ required: true }]}>
+          <InputNumber step="0.01" />
+        </Form.Item>
+        <Form.Item label="Quantity" name="quantity" rules={[{ required: true }]}>
+          <InputNumber step="1" />
+        </Form.Item>
+        <Form.Item label="Unit Price" name="unit_price" rules={[{ required: true }]}>
+          <InputNumber prefix="$" step="0.01" />
+        </Form.Item>
+        <Form.Item label="Storage Limit" name="storage_limit">
+          <InputNumber step="1" />
+        </Form.Item>
+        <Form.Item label="Comment" name="comment">
+          <Input.TextArea autoSize={{ minRows: 3, maxRows: 5 }} />
+        </Form.Item>
+      </Form>
+    </Create>
   );
 };
--- frontend/inventory/src/pages/lenses/show.tsx
+++ frontend/inventory/src/pages/lenses/show.tsx
@@ -1,4 +1,13 @@
 import { useShow } from "@refinedev/core";
+import {
+  TextField,
+  NumberField,
+  DateField,
+  MarkdownField,
+  Show,
+} from "@refinedev/antd";
+
+import { Typography } from "antd";
 
 export const LensShow = () => {
   const {
@@ -10,9 +19,45 @@ export const LensShow = () => {
   }
 
   return (
-    <div>
-      Lens: {data?.data.id}, {data?.data.lens_type}, {data?.data.sphere},{" "}
-      {data?.data.cylinder}
-    </div>
+    <Show isLoading={isLoading} title="Show Lens">
+      <Typography.Title level={5}>Id</Typography.Title>
+      <TextField value={data?.data?.id} />
+
+      <Typography.Title level={5}>Lens Type</Typography.Title>
+      <TextField value={data?.data?.lens_type} />
+
+      <Typography.Title level={5}>Sphere</Typography.Title>
+      <NumberField
+        value={data?.data?.sphere}
+        options={{ minimumSignificantDigits: 3 }}
+      />
+
+      <Typography.Title level={5}>Cylinder</Typography.Title>
+      <NumberField
+        value={data?.data?.cylinder}
+        options={{ minimumSignificantDigits: 3 }}
+      />
+
+      <Typography.Title level={5}>Quantity</Typography.Title>
+      <NumberField value={data?.data?.quantity} />
+
+      <Typography.Title level={5}>Unit Price</Typography.Title>
+      <NumberField
+        value={data?.data?.unit_price}
+        options={{ style: "currency", currency: "USD" }}
+      />
+
+      <Typography.Title level={5}>Storage Limit</Typography.Title>
+      <NumberField value={data?.data?.storage_limit} />
+
+      <Typography.Title level={5}>Comment</Typography.Title>
+      <MarkdownField value={data?.data?.comment} />
+
+      <Typography.Title level={5}>Created At</Typography.Title>
+      <DateField value={data?.data?.created_at} format="MM/DD/YYYY hh:mm:ss A" />
+
+      <Typography.Title level={5}>Updated At</Typography.Title>
+      <DateField value={data?.data?.updated_at} format="MM/DD/YYYY hh:mm:ss A" />
+    </Show>
   );
 };
